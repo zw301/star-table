83aaf6de29d1744a648d1e4a487641d9e37d6d07
Ziyan Wang <zw301@nyu.edu>
2018-04-03 14:23:17 -0700

    Skeleton

---
+++ db/migrate/20180403204712_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :phone_number
+      t.string :password_digest, null: false
+      t.string :session_token, null:false
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+    add_index :users, :email, unique: true
+  end
+end
