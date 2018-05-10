7bf51f0f501a96a57c700505a76694b64b655ea8
Ziyan Wang <zw301@nyu.edu>
2018-05-10 11:29:14 -0700

    Add Michelin-Starred in header

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -13,9 +13,13 @@ const NavBar = ({ currentUser, login, logout, openModal, history }) => {
   const sessionLinks = () => (
     <nav className="navbar-container">
       <div className = "navbar-session">
-        <Link to="/" className="top-bar-logo-link">
-          <h1 className="top-bar-logo-name">StarTable</h1>
-        </Link>
+        <div className="nav-header">
+          <Link to="/" className="top-bar-logo-link">
+            <h1 className="top-bar-logo-name">StarTable</h1>
+          </Link>
+          <p> Find Michelin-Starred Restaurants Near You</p>
+        </div>
+
         <div className="nav-session">
           <button className="btn" onClick={() => openModal('signup')}>Sign up</button>
           <button className="btn" onClick={() => openModal('login')}>Sign in</button>
@@ -31,9 +35,12 @@ const NavBar = ({ currentUser, login, logout, openModal, history }) => {
   const personalGreeting = () => (
     <nav className="navbar-container">
       <div className = "navbar-session">
+      <div className="nav-header">
         <Link to="/" className="top-bar-logo-link">
           <h1 className="top-bar-logo-name">StarTable</h1>
         </Link>
+        <p> Find Michelin-Starred Restaurants Near You</p>
+      </div>
         <div className="nav-greeting">
           <h2 className="header-name">Hi, {currentUser.firstName}</h2>
           <Link className="btn" id="btn-profile" to={`/users/${currentUser.id}`}>My Profile</Link>
